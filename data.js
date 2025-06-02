9dfa35f98d1a945dd2b940730679dddc4d75cbdb
spenwajos <kurikulumspenwajos@gmail.com>
2025-06-02 13:07:45 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+
+const dataSiswa = [
+  {
+    nisn: "1234567890",
+    nis: "2024001",
+    nama: "ANDI PUTRA",
+    tempat_lahir: "Pacitan",
+    tanggal_lahir: "01-01-2010",
+    orang_tua: "BUDI PUTRA",
+    nilai: "87.5",
+    status: "LULUS"
+  },
+  {
+    nisn: "0987654321",
+    nis: "2024002",
+    nama: "SITI AMINAH",
+    tempat_lahir: "Arjosari",
+    tanggal_lahir: "15-02-2010",
+    orang_tua: "JOKO AMIN",
+    nilai: "88.0",
+    status: "LULUS"
+  }
+];
